92e670d90299ef0430333618a161ba3696b7e777
atif-maqsood <37074067+atif-maqsood@users.noreply.github.com>
2020-05-22 20:05:52 +0100

    Updated func pointer
    Added vector value printing example of vector pointer.

--- FunctionPointer.cpp
+++ FunctionPointer.cpp
@@ -14,3 +14,23 @@ int main() {
   
  // std::cout << "Hello World!\n";
 }
+/*
+#include <iostream>
+#include <vector>
+void PrintValue(int value)
+{
+ std::cout << " value is = " << value << std::endl;
+}
+
+void ForEach( const std::vector<int>& values, void(*func)(int))
+{
+  for(int value : values)
+       func(value);
+ 
+}
+
+int main() {
+std::vector<int> values = {1,2,3,4,5};
+ForEach(values,PrintValue);
+}
+*/
